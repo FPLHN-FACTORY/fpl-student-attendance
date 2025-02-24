577204fc65786e43e64370a7b7bd417cd3533882
InuHa98 <bimbimskim@gmail.com>
2025-02-24 10:49:10 +0700

    update base FE

--- Frontend/src/constants/adminConstant.js
+++ Frontend/src/constants/adminConstant.js
@@ -1,7 +1,1 @@
-export const ROUTE_NAMES = {
-  MANAGEMENT_FACILITY: 'route_admin_management_facility',
-  MANAGEMENT_SEMESTER: 'route_admin_management_semester',
-  MANAGEMENT_SUBJECT: 'route_admin_management_subject',
-  MANAGEMENT_LEVEL_PROJECT: 'route_admin_management_level_project',
-  MANAGEMENT_STAFF: 'route_admin_management_staff',
-}
+
--- Frontend/src/router/staffRoute.js
+++ Frontend/src/router/staffRoute.js
@@ -1,6 +1,11 @@
-import { ROUTE_NAMES } from '@/constants/staffConstant'
 import { ROLE } from '@/constants/roleConstant'
 import { GLOBAL_ROUTE_NAMES } from '@/constants/routesConstant'
+
+export const ROUTE_NAMES = {
+  MANAGEMENT_PROJECT: 'route_staff_management_project',
+  MANAGEMENT_PLAN: 'route_staff_management_plan',
+  MANAGEMENT_STUDENT: 'route_staff_management_student',
+}
 
 export const StaffRoutes = [
   {
