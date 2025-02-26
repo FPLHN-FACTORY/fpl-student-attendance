05a783ae92b07e7641b949fedcfad4ff0b4850bc
quocanhh86 <160404811+quocanhh86@users.noreply.github.com>
2025-02-27 00:57:36 +0700

    Facility Management

---
+++ Backend/src/main/java/udpm/hn/studentattendance/core/admin/staff/controller/AdStaffRestController.java
@@ -0,0 +1,42 @@
+package udpm.hn.studentattendance.core.admin.staff.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import udpm.hn.studentattendance.core.admin.staff.model.request.AdCreateUpdateStaffRequest;
+import udpm.hn.studentattendance.core.admin.staff.model.request.AdStaffRequest;
+import udpm.hn.studentattendance.core.admin.staff.service.AdStaffService;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/api/staffs")
+public class AdStaffRestController {
+    private AdStaffService adStaffService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllStaffs(AdStaffRequest staffRequest) {
+        return adStaffService.getAllStaffByFilter(staffRequest);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getStaffById(@PathVariable("id") String id) {
+        return adStaffService.getStaffById(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createStaff(@Valid @RequestBody AdCreateUpdateStaffRequest createUpdateStaffRequest) {
+        return adStaffService.createStaff(createUpdateStaffRequest);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateStaff(@Valid @RequestBody AdCreateUpdateStaffRequest createUpdateStaffRequest) {
+        return adStaffService.updateStaff(createUpdateStaffRequest);
+    }
+
+    @PutMapping("/status/{id}")
+    public ResponseEntity<?> changeStaffStatus(@PathVariable("id") String id) {
+        return adStaffService.changeStaffStatus(id);
+    }
+}
