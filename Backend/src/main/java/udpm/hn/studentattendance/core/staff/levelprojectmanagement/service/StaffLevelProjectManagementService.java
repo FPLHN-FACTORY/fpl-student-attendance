50aabe684737f5f18462d32f64405072a02a5408
hatun <hatun35.dev@gmail.com>
2025-02-27 00:07:42 +0700

    Done View Staff Level Project

--- Backend/src/main/java/udpm/hn/studentattendance/core/staff/levelprojectmanagement/service/StaffLevelProjectManagementService.java
+++ Backend/src/main/java/udpm/hn/studentattendance/core/staff/levelprojectmanagement/service/StaffLevelProjectManagementService.java
@@ -1,6 +1,7 @@
 package udpm.hn.studentattendance.core.staff.levelprojectmanagement.service;
 
 import udpm.hn.studentattendance.core.staff.levelprojectmanagement.model.request.LevelProjectCreateRequest;
+import udpm.hn.studentattendance.core.staff.levelprojectmanagement.model.request.LevelProjectUpdateRequest;
 import udpm.hn.studentattendance.core.staff.levelprojectmanagement.model.request.StaffLevelProjectSearchRequest;
 import udpm.hn.studentattendance.infrastructure.common.ResponseObject;
 
@@ -10,7 +11,7 @@ public interface StaffLevelProjectManagementService {
 
     ResponseObject<?> createLevelProject(LevelProjectCreateRequest request);
 
-    ResponseObject<?> updateLevelProject(String id,LevelProjectCreateRequest request);
+    ResponseObject<?> updateLevelProject(String id, LevelProjectUpdateRequest request);
 
     ResponseObject<?> detailLevelProject(String id);
 
