8ecba1ffde7d5237e850816b2736f77b6e8094cd
quocanhh86 <160404811+quocanhh86@users.noreply.github.com>
2025-02-27 01:20:33 +0700

    Facility Management

--- Backend/src/main/java/udpm/hn/studentattendance/core/admin/staff/model/request/AdStaffRoleRequest.java
+++ Backend/src/main/java/udpm/hn/studentattendance/core/admin/staff/model/request/AdStaffRoleRequest.java
@@ -2,10 +2,11 @@ package udpm.hn.studentattendance.core.admin.staff.model.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import udpm.hn.studentattendance.infrastructure.common.PageableRequest;
 
 @Getter
 @Setter
-public class AdStaffRoleRequest {
+public class AdStaffRoleRequest extends PageableRequest {
     private String staffId;
 
     private String idFacility;
---
+++ Backend/src/main/java/udpm/hn/studentattendance/core/admin/staff/model/response/AdCheckStaffRoleResponse.java
@@ -0,0 +1,13 @@
+package udpm.hn.studentattendance.core.admin.staff.model.response;
+
+public interface AdCheckStaffRoleResponse {
+    String getRoleName();
+
+    String getRoleCode();
+
+    String getIdRole();
+
+    String getFacilityName();
+
+    Boolean getChecked();
+}
