cbfcac5ddabafa8b4d94de363709059740281ed3
hatun <hatun35.dev@gmail.com>
2025-03-11 22:09:19 +0700

    Download template and import excel - Admin - Project

---
+++ Backend/src/main/java/udpm/hn/studentattendance/core/admin/project/model/request/ProjectUpdateRequest.java
@@ -0,0 +1,24 @@
+package udpm.hn.studentattendance.core.admin.project.model.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ProjectUpdateRequest {
+
+    private String name;
+
+    private String description;
+
+    private String idLevelProject;
+
+    private String idSemester;
+
+    private String idSubject;
+
+    private String status;
+
+}
