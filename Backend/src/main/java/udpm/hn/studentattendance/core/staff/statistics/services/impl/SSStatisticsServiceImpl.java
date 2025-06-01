1f6b1bb973fed7e17a1665fb697c4e8bc88e96dc
InuHa98 <bimbimskim@gmail.com>
2025-06-02 00:56:44 +0700

    Fix export attendance

--- Backend/src/main/java/udpm/hn/studentattendance/core/staff/statistics/services/impl/SSStatisticsServiceImpl.java
+++ Backend/src/main/java/udpm/hn/studentattendance/core/staff/statistics/services/impl/SSStatisticsServiceImpl.java
@@ -45,7 +45,10 @@ import udpm.hn.studentattendance.utils.ExcelUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
@@ -191,7 +194,10 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
                         .map(this::buildCellPlanDate)
                         .collect(Collectors.toSet());
                 List<String> lstPlanDate = stPlanDate.stream()
-                        .sorted()
+                        .sorted(Comparator.comparing(s -> {
+                            String datePart = s.split(" - ")[0];
+                            return LocalDate.parse(datePart, DateTimeFormatter.ofPattern(DateTimeUtils.DATE_FORMAT.replace('/', '-')));
+                        }))
                         .toList();
 
                 Set<ExStudentModel> stPStudent = lstData.stream()
@@ -203,11 +209,13 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
                 headers.addAll(lstPlanDate);
                 headers.add("Tổng");
                 headers.add("Vắng(%)");
+                headers.add("Điểm danh bù(lần)");
 
                 Sheet sheet = ExcelUtils.createTemplate(workbook, factory.getName(), headers, new ArrayList<>());
 
                 Map<Object, String> colorMap = new HashMap<>();
                 colorMap.put("Có mặt", "#a9d08e");
+                colorMap.put("Có mặt (bù)", "#ffd966");
                 colorMap.put("Vắng mặt", "#ff7d7d");
 
 
@@ -221,6 +229,7 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
                     dataCell.add(studentName);
 
                     int total_absent = 0;
+                    int total_recovery = 0;
                     for(String namePlanDate: lstPlanDate) {
                         SSPlanDateStudentFactoryResponse planDate = lstData.stream().filter(s -> s.getCode().equals(studentCode) && buildCellPlanDate(s).equals(namePlanDate)).findFirst().orElse(null);
                         if (planDate == null || planDate.getStartDate() > DateTimeUtils.getCurrentTimeMillis()) {
@@ -228,7 +237,12 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
                             continue;
                         }
                         if (planDate.getStatus() == AttendanceStatus.PRESENT.ordinal()) {
-                            dataCell.add("Có mặt");
+                            if (planDate.getLateCheckin() != null && planDate.getLateCheckin() > 0 || planDate.getLateCheckout() != null && planDate.getLateCheckout() > 0) {
+                                total_recovery++;
+                                dataCell.add("Có mặt (bù)");
+                            } else {
+                                dataCell.add("Có mặt");
+                            }
                         } else {
                             total_absent++;
                             dataCell.add("Vắng mặt");
@@ -237,6 +251,7 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
 
                     dataCell.add(total_absent + "/" + lstPlanDate.size());
                     dataCell.add(Math.round((double) total_absent / lstPlanDate.size() * 1000) / 10.0 + "%");
+                    dataCell.add(total_recovery);
 
                     ExcelUtils.insertRow(sheet, row, dataCell, colorMap);
                     row++;
@@ -252,7 +267,7 @@ public class SSStatisticsServiceImpl implements SSStatisticsService {
     }
 
     private String buildCellPlanDate(SSPlanDateStudentFactoryResponse o) {
-        return DateTimeUtils.convertMillisToDate(o.getStartDate(), "dd-MM-yyyy") + " - Ca " + o.getShift();
+        return DateTimeUtils.convertMillisToDate(o.getStartDate(), DateTimeUtils.DATE_FORMAT.replace('/', '-')) + " - Ca " + o.getShift();
     }
 
 }
