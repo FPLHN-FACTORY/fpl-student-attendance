a777ca29a62ae44fce02ec12526f7d4a9a2187d7
quocanhh86 <quocanhn352@gmail.com>
2025-06-26 15:50:54 +0700

    done send email shedule plandate to teacher and student

--- Backend/src/main/java/udpm/hn/studentattendance/repositories/PlanDateRepository.java
+++ Backend/src/main/java/udpm/hn/studentattendance/repositories/PlanDateRepository.java
@@ -6,4 +6,6 @@ import udpm.hn.studentattendance.entities.PlanDate;
 
 @Repository
 public interface PlanDateRepository extends JpaRepository<PlanDate, String> {
+    // Basic CRUD operations are inherited from JpaRepository
+    // Specific queries have been moved to ScheduleReminderRepository
 }
