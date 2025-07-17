a6f5e53cb4e27fada85771a16eec7d4b6cbbe7c5
InuHa98 <bimbimskim@gmail.com>
2025-07-18 01:47:19 +0700

    Change overwrite toString to lombok

--- Backend/src/main/java/udpm/hn/studentattendance/core/admin/facility/model/request/AFFacilitySearchRequest.java
+++ Backend/src/main/java/udpm/hn/studentattendance/core/admin/facility/model/request/AFFacilitySearchRequest.java
@@ -3,26 +3,19 @@ package udpm.hn.studentattendance.core.admin.facility.model.request;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import udpm.hn.studentattendance.infrastructure.common.PageableRequest;
 import udpm.hn.studentattendance.infrastructure.constants.EntityProperties;
 import udpm.hn.studentattendance.infrastructure.constants.EntityStatus;
 
 @Getter
 @Setter
+@ToString
 public class AFFacilitySearchRequest extends PageableRequest {
-    @Size(max = EntityProperties.LENGTH_CODE, message = "Từ khóa không được vượt quá 50 ký tự")
+
+    @Size(max = EntityProperties.LENGTH_CODE, message = "Từ khóa không được vượt quá " + EntityProperties.LENGTH_CODE + " ký tự")
     private String name;
 
     private EntityStatus status;
 
-    @Override
-    public String toString() {
-        return "page=" + getPage() +
-                "_size=" + getSize() +
-                "_orderBy=" + getOrderBy() +
-                "_sortBy=" + getSortBy() +
-                "_q=" + (getQ() != null ? getQ() : "") +
-                "_name=" + (name != null ? name : "") +
-                "_status=" + (status != null ? status : "");
-    }
 }
--- Backend/src/main/java/udpm/hn/studentattendance/core/staff/attendancerecovery/model/request/STAttendanceRecoveryRequest.java
+++ Backend/src/main/java/udpm/hn/studentattendance/core/staff/attendancerecovery/model/request/STAttendanceRecoveryRequest.java
@@ -2,10 +2,12 @@ package udpm.hn.studentattendance.core.staff.attendancerecovery.model.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import udpm.hn.studentattendance.infrastructure.common.PageableRequest;
 
 @Getter
 @Setter
+@ToString
 public class STAttendanceRecoveryRequest extends PageableRequest {
 
     private String searchQuery;
@@ -16,16 +18,4 @@ public class STAttendanceRecoveryRequest extends PageableRequest {
 
     private String semesterId;
 
-    @Override
-    public String toString() {
-        return "page=" + getPage() +
-                "_size=" + getSize() +
-                "_orderBy=" + getOrderBy() +
-                "_sortBy=" + getSortBy() +
-                "_q=" + (getQ() != null ? getQ() : "") +
-                "_searchQuery=" + (searchQuery != null ? searchQuery : "") +
-                "_fromDate=" + (fromDate != null ? fromDate : "") +
-                "_toDate=" + (toDate != null ? toDate : "") +
-                "_semesterId=" + (semesterId != null ? semesterId : "");
-    }
 }
