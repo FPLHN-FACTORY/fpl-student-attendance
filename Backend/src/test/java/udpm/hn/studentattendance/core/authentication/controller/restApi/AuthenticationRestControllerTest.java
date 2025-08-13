78c26bd46a17937caa0631cdaa30fb9d7e6d2845
quocanhh86 <quocanhn352@gmail.com>
2025-08-13 11:35:23 +0700

    merge origin dev

--- Backend/src/test/java/udpm/hn/studentattendance/core/authentication/controller/restApi/AuthenticationRestControllerTest.java
+++ Backend/src/test/java/udpm/hn/studentattendance/core/authentication/controller/restApi/AuthenticationRestControllerTest.java
@@ -8,7 +8,6 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 import udpm.hn.studentattendance.core.authentication.model.request.AuthenticationStudentRegisterRequest;
-import udpm.hn.studentattendance.core.authentication.model.request.AuthenticationStudentUpdateFaceIDRequest;
 import udpm.hn.studentattendance.core.authentication.model.request.AuthenticationToken;
 import udpm.hn.studentattendance.core.authentication.services.AuthenticationService;
 import udpm.hn.studentattendance.infrastructure.constants.SettingKeys;
